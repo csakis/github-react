870534663bb8a0ace1301cabfb797af6ff57660b
Csaba Kiss <csakis@gmail.com>
2021-10-19 21:32:34 -0600

    added api request to github

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,9 @@ function App() {
 
   const sendSearch = async (e) => {
     e.preventDefault();
-    const response = await fetch(`https://api.github.com/?`)
+    const response = await fetch(`https://api.github.com/search/repositories?q=${query}+language:javascript&per_page=10`)
+    const json = await response.json();
+    setResults(json.items)
   }
 
 
